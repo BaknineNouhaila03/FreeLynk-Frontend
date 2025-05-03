14a15cf0e47071c6447c061324376f840e0d134b
unknown <bakninenouha@gmail.com>
2025-05-03 14:29:24 +0100

    client_home

--- freelynk/src/app/page.jsx
+++ freelynk/src/app/page.jsx
@@ -1,6 +1,7 @@
 
 import Home from '@/components/home/Home'; 
-
+import Home_client from '@/components/Home_client/Home_client'; 
+import Navbar from '@/components/navbar/Navbar'
 export default function IndexPage() {
-  return <Home />;
+  return <Home_client />
 }
--- freelynk/src/components/navbar/Navbar.jsx
+++ freelynk/src/components/navbar/Navbar.jsx
@@ -1,10 +1,61 @@
-import { FaSearch, FaBell, FaHome, FaUser, FaCommentDots, FaBookmark } from 'react-icons/fa';
+"use client"
 
-export default function Navbar() {
-  return (
-    <div>
-       <div style={{ backgroundColor: '#f0f0f0' , width:"300px" ,marginTop:"100px"}} />
-       <h>Hello</h>
-    </div>
-  );
+import { useState, useEffect } from 'react';
+import Link from 'next/link';
+import styles from '../NavBar.module.css';
+
+import { FiSearch } from "react-icons/fi";
+import { AiOutlineHome } from "react-icons/ai";
+import { IoMdNotificationsOutline } from "react-icons/io";
+import { BsChatDots } from "react-icons/bs";
+import { BsBookmarkFill } from "react-icons/bs";
+import { FaUser } from "react-icons/fa";
+
+export default function NavBar() {
+    const [scrolled, setScrolled] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const isScrolled = window.scrollY > 50;
+            if (isScrolled !== scrolled) {
+                setScrolled(isScrolled);
+            }
+        };
+        window.addEventListener('scroll', handleScroll);
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, [scrolled]);
+
+    return (
+        <nav className={`${styles.navbar} ${scrolled ? styles.scrolled : ''}`} style={{ backgroundColor: "#e6e6e6"}}>
+            <div className={styles.logo}>
+                <Link href="/">
+                    <img src="/assets/FreeLynk.png" alt="Logo" />
+                </Link>
+            </div>
+            <div className={styles.searchContainer}>
+                <FiSearch className={styles.searchIcon} />
+                <input
+                    type="text"
+                    placeholder="What service are you looking for today ?"
+                    className={styles.searchInput}
+                />
+            </div>
+
+            {/* ICON NAVIGATION */}
+            <ul className={styles.iconNav}>
+                <li><Link href="/"><AiOutlineHome size={24} /></Link></li>
+                <li style={{ position: 'relative' }}>
+                    <Link href="/notifications">
+                        <IoMdNotificationsOutline size={24} />
+                        <span className={styles.notificationDot}></span>
+                    </Link>
+                </li>
+                <li><Link href="/messages"><BsChatDots size={22} /></Link></li>
+                <li><Link href="/saved"><BsBookmarkFill size={20} /></Link></li>
+                <li><Link href="/profile"><FaUser size={20} /></Link></li>
+            </ul>
+
+            {/* Optional mobile menu toggle if you want */}
+        </nav>
+    );
 }
