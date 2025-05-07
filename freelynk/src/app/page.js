e66edf7d3b03eed8b9956b99f1049c89df0e0a8f
hafsa talbi <hafsatllb@gmail.com>
2025-05-07 21:25:58 +0100

    SavedProjects

--- freelynk/src/app/page.js
+++ freelynk/src/app/page.js
@@ -5,10 +5,11 @@ import Home_client from '@/components/home_client/Home_client';
 import MyProjects from '@/components/MyProjects/MyProjects';
 import ProjectDetails from '@/components/ProjectDetails/ProjectDetails';
 import SavedFreelancers from '@/components/SavedFreelancers/SavedFreelancers';
+import SavedProjects from '@/components/SavedProjects/SavedProjects';
 import Settings from '@/components/Settings/Settings';
 import ProfilePage from '@/components/signup-freelancer/ProfilePage';
 
 
 export default function IndexPage() {
-  return <ProfilePage/>;
+  return <SavedProjects/>;
 }
