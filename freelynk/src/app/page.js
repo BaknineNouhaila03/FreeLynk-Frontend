623e5b581b592c57c98de466364f719356c45f3a
unknown <bakninenouha@gmail.com>
2025-05-08 12:33:31 +0100

    gigDetails

--- freelynk/src/app/page.js
+++ freelynk/src/app/page.js
@@ -1,6 +1,7 @@
 import AddProject from '@/components/AddProject/AddProject';
 import BrowseProjects from '@/components/BrowseProjects/BrowseProjects';
 import ChatInterface from '@/components/Chat/chat';
+import GigDetails from '@/components/GigDetails/GigDetails';
 import Home_client from '@/components/home_client/Home_client';
 import MyProjects from '@/components/MyProjects/MyProjects';
 import ProjectDetails from '@/components/ProjectDetails/ProjectDetails';
@@ -9,5 +10,5 @@ import Settings from '@/components/Settings/Settings';
 
 
 export default function IndexPage() {
-  return <ChatInterface/>;
+  return <GigDetails/>;
 }
