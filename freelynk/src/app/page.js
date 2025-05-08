cbb467d726a2cc5e13f7815b1010137d468261a8
unknown <bakninenouha@gmail.com>
2025-05-08 13:11:03 +0100

    home_freelancer

--- freelynk/src/app/page.js
+++ freelynk/src/app/page.js
@@ -3,6 +3,7 @@ import BrowseProjects from '@/components/BrowseProjects/BrowseProjects';
 import ChatInterface from '@/components/Chat/chat';
 import GigDetails from '@/components/GigDetails/GigDetails';
 import Home_client from '@/components/home_client/Home_client';
+import Home_Freelancer from '@/components/Home_Freelancer/Home_Freelancer';
 import MyProjects from '@/components/MyProjects/MyProjects';
 import ProjectDetails from '@/components/ProjectDetails/ProjectDetails';
 import SavedFreelancers from '@/components/SavedFreelancers/SavedFreelancers';
@@ -10,5 +11,5 @@ import Settings from '@/components/Settings/Settings';
 
 
 export default function IndexPage() {
-  return <GigDetails/>;
+  return <BrowseProjects/>;
 }
